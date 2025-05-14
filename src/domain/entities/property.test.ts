7835e1aa5f2901053ef954f06aabdcb7962bb720
alineAssuncao <aline.jassuncao@gmail.com>
2025-05-14 20:53:49 -0300

    atualizações

---
+++ src/domain/entities/property.test.ts
@@ -0,0 +1,12 @@
+import { Property } from "./property";
+
+describe("Property Entity", () => {
+    it("deve criar uma instancia de Property com todo os atritutos", () => {
+        const property = new Property("1", "Casa de campo", "Casa tranquila, junto a natureza", 4, 150);
+        expect(property.getId()).toBe("1");
+        expect(property.getName()).toBe("Casa de campo");
+        expect(property.getDescription()).toBe("Casa tranquila, junto a natureza");
+        expect(property.geMaxGuests()).toBe(4);
+        expect(property.getBasePrice()).toBe(150);
+    });
+});
---
+++ src/domain/entities/property.ts
@@ -0,0 +1,37 @@
+export class Property{
+    constructor(
+        private id: string,
+        private name: string,
+        private description: string,
+        private maxGuests: number,
+        private basePricePerNight: number
+    ){
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.maxGuests = maxGuests;
+        this.basePricePerNight = basePricePerNight;
+    }
+
+    
+    getId(): string{
+        return this.id;
+    }
+
+    getName(): string{
+        return this.name;
+    }
+
+    getDescription(): string{
+        return this.description;
+    }
+
+    geMaxGuests(): number{
+        return this.maxGuests;
+    }
+
+    getBasePrice(): number{
+        return this.basePricePerNight;
+    }
+    
+}
