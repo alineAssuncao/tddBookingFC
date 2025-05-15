ef43098ec71ae1e2a134bdce005b9bf92177a3ce
alineAssuncao <aline.jassuncao@gmail.com>
2025-05-15 19:55:24 -0300

    alterações

--- src/domain/entities/property.test.ts
+++ src/domain/entities/property.test.ts
@@ -2,11 +2,11 @@ import { Property } from "./property";
 
 describe("Property Entity", () => {
     it("deve criar uma instancia de Property com todo os atritutos", () => {
-        const property = new Property("1", "Casa de campo", "Casa tranquila, junto a natureza", 4, 150);
+        const property = new Property("1", "Casa de campo", "Casa tranquila, junto a natureza", 4, 175);
         expect(property.getId()).toBe("1");
         expect(property.getName()).toBe("Casa de campo");
         expect(property.getDescription()).toBe("Casa tranquila, junto a natureza");
         expect(property.geMaxGuests()).toBe(4);
-        expect(property.getBasePrice()).toBe(150);
+        expect(property.getBasePrice()).toBe(175);
     });
 });
